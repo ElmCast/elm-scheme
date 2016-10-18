5d9811886ad124660132ca52d8ec2d71752fb128
Joseph Hager <ajhager@gmail.com>
2016-10-18 02:37:54 -0500

    Add the rest of the parsers for part 2

--- Part2.elm
+++ Part2.elm
@@ -4,12 +4,17 @@ import Html exposing (text)
 import Combine exposing (..)
 import Combine.Char as Combine exposing (..)
 import Combine.Infix as Combine exposing (..)
+import Combine.Num as Combine exposing (int)
 import String
 
 
-symbol : Parser Char
-symbol =
-    Combine.oneOf <| String.toList "!#$%&|*+-/:<=>?@^_~"
+type LispVal
+    = Atom String
+    | List (List LispVal)
+    | DottedList (List LispVal) LispVal
+    | Number Int
+    | String String
+    | Bool Bool
 
 
 spaces : Parser ()
@@ -17,9 +22,96 @@ spaces =
     skipMany1 space
 
 
+symbol : Parser Char
+symbol =
+    Combine.oneOf <| String.toList "!#$%&|*+-/:<=>?@^_~"
+
+
+parseString : Parser LispVal
+parseString =
+    String <$> (String.fromList <$> (char '"' *> many (noneOf [ '"' ]) <* char '"'))
+
+
+parseBoolOrAtom : Parser LispVal
+parseBoolOrAtom =
+    let
+        start =
+            lower <|> upper <|> symbol
+
+        rest =
+            many (lower <|> upper <|> digit <|> symbol)
+
+        atom string =
+            case string of
+                "#t" ->
+                    Bool True
+
+                "#f" ->
+                    Bool False
+
+                _ ->
+                    Atom string
+    in
+        atom <$> (String.fromList <$> ((::) <$> start <*> rest))
+
+
+parseNumber : Parser LispVal
+parseNumber =
+    Number <$> int
+
+
+parseList : Parser LispVal
+parseList =
+    let
+        list =
+            List <$> (sepBy spaces parseExpr)
+    in
+        char '(' *> list <* char ')'
+
+
+parseDottedList : Parser LispVal
+parseDottedList =
+    let
+        head =
+            sepEndBy spaces parseExpr
+
+        tail =
+            char '.' *> spaces *> parseExpr
+
+        list =
+            DottedList <$> head <*> tail
+    in
+        char '(' *> list <* char ')'
+
+
+parseQuoted : Parser LispVal
+parseQuoted =
+    rec <|
+        \() ->
+            let
+                quoted e =
+                    [ Atom "quote", e ]
+            in
+                List <$> (quoted <$> (char '\'' *> parseExpr))
+
+
+parseExpr : Parser LispVal
+parseExpr =
+    rec <|
+        \() ->
+            choice
+                [ parseBoolOrAtom
+                , parseString
+                , parseNumber
+                , parseQuoted
+                , parseList
+                , parseDottedList
+                ]
+
+
 readExpr : String -> String
 readExpr input =
-    case parse (spaces *> symbol) input of
+    case parse parseExpr input of
         ( Ok value, _ ) ->
             "Found value: " ++ (toString value)
 
@@ -28,4 +120,4 @@ readExpr input =
 
 
 main =
-    text (readExpr " !")
+    text (readExpr "(a \"Hello\" . ('c #f 4))")
